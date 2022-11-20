4b21fba34cb3c03c3f375c13ad41e6912fe94eb7
Gengyuan Huang <scotthuang007@outlook.com>
2022-11-19 20:03:02 -0700

    Update project structure

---
+++ src/components/Desktop.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="Desktop">
+    <div class="Desktop__windows">
+      <WindowsContainer />
+    </div>
+    <div class="Desktop__action-bar">
+      <ActionBar />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.Desktop {
+  height: 100%;
+  width: 100%;
+
+  &__windows {
+    height: 80%;
+  }
+
+  &__action-bar {
+    height: 20%;
+  }
+}
+</style>
+
+<script setup lang="ts">
+import ActionBar from "./ActionBar.vue";
+import WindowsContainer from "./WindowsContainer.vue";
+</script>
