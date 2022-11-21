c1ff46fcfef4fa4d191eeed137c9869a164550a0
Gengyuan Huang <scotthuang007@outlook.com>
2022-11-20 19:28:01 -0700

    Implement custom events on MovingWindow component

--- src/components/MovingWindow.vue
+++ src/components/MovingWindow.vue
@@ -3,14 +3,45 @@ import MovingWindowContent from "./MovingWindowContent.vue";
 import MovingWindowTitleBar from "./MovingWindowTitleBar.vue";
 import { computed } from "@vue/reactivity";
 
+// type
+export type MovingWindowResizeDirection =
+  | "se"
+  | "sw"
+  | "ne"
+  | "nw"
+  | "n"
+  | "w"
+  | "s"
+  | "e";
+
+export interface EventInfoResize {
+  id: string;
+  direction: MovingWindowResizeDirection;
+}
+
+export interface EventInfoMove {
+  id: string;
+}
+
 // define props
 const props = defineProps<{
+  id: string;
   position: [number, number];
   size: [number, number];
 }>();
 
+// define events
+const emits = defineEmits<{
+  (e: "movingWindowResizeStart", eventInfo: EventInfoResize): void;
+  (e: "movingWindowResizeEnd", eventInfo: EventInfoResize): void;
+  (e: "movingWindowMoveStart", eventInfo: EventInfoMove): void;
+  (e: "movingWindowMoveEnd", eventInfo: EventInfoMove): void;
+}>();
+
 // compute styling string
-const styleWindowPositionLeft = computed(() => String(props.position[0]) + "px");
+const styleWindowPositionLeft = computed(
+  () => String(props.position[0]) + "px"
+);
 const styleWindowPositionTop = computed(() => String(props.position[1]) + "px");
 const styleWindowSizeWidth = computed(() => String(props.size[0]) + "px");
 const styleWindowSizeHeight = computed(() => String(props.size[1]) + "px");
@@ -18,22 +49,146 @@ const styleWindowSizeHeight = computed(() => String(props.size[1]) + "px");
 
 <template>
   <div class="MovingWindow">
-    <div class="MovingWindow__title_bar">
+    <div
+      class="MovingWindow__title_bar"
+      @mousedown="emits('movingWindowMoveStart', { id: props.id })"
+      @touchstart="emits('movingWindowMoveStart', { id: props.id })"
+      @mouseup="emits('movingWindowMoveEnd', { id: props.id })"
+      @touchend="emits('movingWindowMoveEnd', { id: props.id })"
+    >
       <MovingWindowTitleBar />
     </div>
     <div class="MovingWindow__content">
       <MovingWindowContent />
     </div>
 
-    <div class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-se"></div>
-    <div class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-sw"></div>
-    <div class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-ne"></div>
-    <div class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-nw"></div>
+    <div
+      class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-se"
+      @mousedown="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'se' })
+      "
+      @touchstart="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'se' })
+      "
+      @mouseup="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'se' })
+      "
+      @touchend="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'se' })
+      "
+    ></div>
 
-    <div class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-n"></div>
-    <div class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-w"></div>
-    <div class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-s"></div>
-    <div class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-e"></div>
+    <div
+      class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-sw"
+      @mousedown="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'sw' })
+      "
+      @touchstart="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'sw' })
+      "
+      @mouseup="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'sw' })
+      "
+      @touchend="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'sw' })
+      "
+    ></div>
+
+    <div
+      class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-ne"
+      @mousedown="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'ne' })
+      "
+      @touchstart="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'ne' })
+      "
+      @mouseup="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'ne' })
+      "
+      @touchend="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'ne' })
+      "
+    ></div>
+
+    <div
+      class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-nw"
+      @mousedown="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'nw' })
+      "
+      @touchstart="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'nw' })
+      "
+      @mouseup="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'nw' })
+      "
+      @touchend="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'nw' })
+      "
+    ></div>
+
+    <div
+      class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-n"
+      @mousedown="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'n' })
+      "
+      @touchstart="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'n' })
+      "
+      @mouseup="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'n' })
+      "
+      @touchend="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'n' })
+      "
+    ></div>
+
+    <div
+      class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-w"
+      @mousedown="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'w' })
+      "
+      @touchstart="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'w' })
+      "
+      @mouseup="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'w' })
+      "
+      @touchend="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'w' })
+      "
+    ></div>
+
+    <div
+      class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-s"
+      @mousedown="
+        emits('movingWindowResizeStart', { id: props.id, direction: 's' })
+      "
+      @touchstart="
+        emits('movingWindowResizeStart', { id: props.id, direction: 's' })
+      "
+      @mouseup="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 's' })
+      "
+      @touchend="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 's' })
+      "
+    ></div>
+
+    <div
+      class="MovingWindow__panel_resize MovingWindow__panel_resize--direction-e"
+      @mousedown="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'e' })
+      "
+      @touchstart="
+        emits('movingWindowResizeStart', { id: props.id, direction: 'e' })
+      "
+      @mouseup="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'e' })
+      "
+      @touchend="
+        emits('movingWindowResizeEnd', { id: props.id, direction: 'e' })
+      "
+    ></div>
   </div>
 </template>
 
--- src/components/WindowsManager.vue
+++ src/components/WindowsManager.vue
@@ -1,49 +1,84 @@
 <script setup lang="ts">
-import MovingWindow from "./MovingWindow.vue";
 import { ref, Ref, onMounted, onUnmounted } from "vue";
 import { useDesktopStatesStore } from "../stores/desktopStates";
+import { v4 as uuid } from "uuid";
+import MovingWindow from "./MovingWindow.vue";
+import {
+  MovingWindowResizeDirection,
+  EventInfoResize,
+  EventInfoMove,
+} from "./MovingWindow.vue";
 
 interface MovingWindowLocalState {
-  position: Ref<[number, number]>
-  size: Ref<[number, number]>
+  id: string;
+  position: Ref<[number, number]>;
+  size: Ref<[number, number]>;
 }
 
 const movingWindows: MovingWindowLocalState[] = [
   {
+    id: uuid(),
     position: ref([20, 50] as [number, number]),
-    size: ref([200, 200] as [number, number])
+    size: ref([200, 200] as [number, number]),
   },
   {
+    id: uuid(),
     position: ref([90, 50] as [number, number]),
-    size: ref([100, 100] as [number, number])
-  }
-]
+    size: ref([100, 100] as [number, number]),
+  },
+];
 
 const desktopStates = useDesktopStatesStore();
 const windowsManagerElement = ref<HTMLDivElement>();
 const handlerWindowResizeUpdateWindowsManagerState = (event: UIEvent) => {
-  if (windowsManagerElement.value !== undefined && windowsManagerElement.value !== null) {
+  if (
+    windowsManagerElement.value !== undefined &&
+    windowsManagerElement.value !== null
+  ) {
     const rect = windowsManagerElement.value.getBoundingClientRect();
-    desktopStates.updateSizeWindowsManager([windowsManagerElement.value.offsetWidth, windowsManagerElement.value.offsetHeight]);
+    desktopStates.updateSizeWindowsManager([
+      windowsManagerElement.value.offsetWidth,
+      windowsManagerElement.value.offsetHeight,
+    ]);
     desktopStates.updatePositionWindowsManager([rect.left, rect.top]);
   }
 };
 
+// methods
+const handlerWindowResizeStart = (e: EventInfoResize) => {};
+const handlerWindowResizeEnd = (e: EventInfoResize) => {};
+const handlerWindowMoveStart = (e: EventInfoMove) => {};
+const handlerWindowMoveEnd = (e: EventInfoMove) => {};
+
 // life cycle
 onMounted(() => {
-  window.addEventListener('resize', handlerWindowResizeUpdateWindowsManagerState);
+  window.addEventListener(
+    "resize",
+    handlerWindowResizeUpdateWindowsManagerState
+  );
 });
 
 onUnmounted(() => {
-  window.removeEventListener('resize', handlerWindowResizeUpdateWindowsManagerState);
+  window.removeEventListener(
+    "resize",
+    handlerWindowResizeUpdateWindowsManagerState
+  );
 });
-
-
 </script>
 
 <template>
   <div class="WindowsManager" ref="windowsManagerElement">
-    <MovingWindow v-for="mw in movingWindows" :position="mw.position.value" :size="mw.size.value" />
+    <MovingWindow
+      v-for="mw in movingWindows"
+      :key="mw.id"
+      :id="mw.id"
+      :position="mw.position.value"
+      :size="mw.size.value"
+      @moving-window-move-start="handlerWindowMoveStart"
+      @moving-window-move-end="handlerWindowMoveEnd"
+      @moving-window-resize-start="handlerWindowResizeStart"
+      @moving-window-resize-end="handlerWindowResizeEnd"
+    />
   </div>
 </template>
 
