1534a6f0d5d622ae14b7bcff72a61859cb29d7f8
Gengyuan Huang <scotthuang007@outlook.com>
2023-02-06 20:58:02 -0700

    Update window config

--- src/constants/WindowManager.ts
+++ src/constants/WindowManager.ts
@@ -3,9 +3,9 @@ import { Tuple } from "../types/TypeBasic";
 export const WINDOW_CONFIG = {
   MIN_WINDOW_VISIABLE_BOARDER: 50, // minial visual parts of a moving window within window manager
   DEFAULT_SIZE_MIN_WINDOW: [300, 400] as Tuple<number>,
-  WIN_INIT_SIZE_PERC: [0.95, 0.95] as Tuple<number>,
+  WIN_INIT_SIZE_PERC: [0.85, 0.95] as Tuple<number>,
   WIN_INIT_SIZE_RATIO: (1 / 1) as number,
-  WIN_INIT_STACK_POSITION_OFFSET: [20, 35] as Tuple<number>,
+  WIN_INIT_STACK_POSITION_OFFSET: [7, 7] as Tuple<number>,
 } as const;
 
 export const POINTER_CONFIG = {
