87db81d5d1f545f762978bef8af8699eb69d701a
Gengyuan Huang <scotthuang007@outlook.com>
2023-02-08 23:02:14 -0700

    Update window init size

--- src/constants/WindowManager.ts
+++ src/constants/WindowManager.ts
@@ -3,7 +3,7 @@ import { Tuple } from "../types/TypeBasic";
 export const WINDOW_CONFIG = {
   MIN_WINDOW_VISIABLE_BOARDER: 50, // minial visual parts of a moving window within window manager
   DEFAULT_SIZE_MIN_WINDOW: [300, 400] as Tuple<number>,
-  WIN_INIT_SIZE_PERC: [0.85, 0.95] as Tuple<number>,
+  WIN_INIT_SIZE_PERC: [0.9, 0.95] as Tuple<number>,
   WIN_INIT_SIZE_RATIO: (1 / 1) as number,
   WIN_INIT_STACK_POSITION_OFFSET: [7, 7] as Tuple<number>,
 } as const;
