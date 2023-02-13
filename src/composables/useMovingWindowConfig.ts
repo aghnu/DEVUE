548eb8071077e10e027452fa7aece46e9a7e68f7
Gengyuan(Scott) Huang <shuang@helcim.com>
2023-02-13 13:25:56 -0700

    Implement snapping without release handling

--- src/composables/useMovingWindowConfig.ts
+++ src/composables/useMovingWindowConfig.ts
@@ -7,18 +7,48 @@ export function useMovingWindowConfig(state: Ref<MovingWindowLocalState>) {
   const windowsState = useWindowsStatesStore();
   const { topWindow } = storeToRefs(windowsState);
 
-  const styleWindowPositionLeft = computed(
-    () => String(state.value.position[0]) + "px"
-  );
-  const styleWindowPositionTop = computed(
-    () => String(state.value.position[1]) + "px"
-  );
-  const styleWindowSizeWidth = computed(
-    () => String(state.value.size[0]) + "px"
-  );
-  const styleWindowSizeHeight = computed(
-    () => String(state.value.size[1]) + "px"
-  );
+  const styleWindowPositionLeft = computed(() => {
+    switch (state.value.snapped) {
+      case "left":
+      case "top":
+        return "0";
+      case "right":
+        return "50%";
+      default:
+        return String(state.value.position[0]) + "px";
+    }
+  });
+  const styleWindowPositionTop = computed(() => {
+    switch (state.value.snapped) {
+      case "left":
+      case "top":
+      case "right":
+        return "0";
+      default:
+        return String(state.value.position[1]) + "px";
+    }
+  });
+  const styleWindowSizeWidth = computed(() => {
+    switch (state.value.snapped) {
+      case "top":
+        return "100%";
+      case "left":
+      case "right":
+        return "50%";
+      default:
+        return String(state.value.size[0]) + "px";
+    }
+  });
+  const styleWindowSizeHeight = computed(() => {
+    switch (state.value.snapped) {
+      case "left":
+      case "top":
+      case "right":
+        return "100%";
+      default:
+        return String(state.value.size[1]) + "px";
+    }
+  });
   const styleWindowZIndex = computed(() => String(state.value.order));
   const isWindowFocused = computed(
     () => topWindow !== null && state.value.id === state.value.id
