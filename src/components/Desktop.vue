c434231772d71a79b1f12680a48e9dcd465d80ee
Gengyuan Huang <gengyuan@ualberta.ca>
2023-04-07 14:03:00 -0600

    Upload wallpaper

--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -112,7 +112,9 @@ onUnmounted(() => {
 
   background-color: var(--color-background-dark);
   // background-color: #060522;
-  // background-image: url(https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80);
+  background-image: url(../assets/img/DEVUE.png);
+  background-position: top;
+  background-size: cover;
 
   &__status_bar {
     height: fit-content;
