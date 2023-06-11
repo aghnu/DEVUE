e7de68d41955f484e7c7abdfe3f453bdbb9f33b4
Gengyuan Huang <gengyuan@ualberta.ca>
2023-06-11 00:36:58 -0600

    Update vite build config to use auto venrdor chunk split

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
-  plugins: [vue()],
+  plugins: [vue(), splitVendorChunkPlugin()],
   css: {
     preprocessorOptions: {
       scss: {
@@ -19,6 +19,7 @@ export default defineConfig({
     },
   },
   build: {
+    emptyOutDir: true,
     chunkSizeWarningLimit: 1000,
   },
 });
